50248413c68d8eb31212b7b12b1850bf53de3f58
novakvova <novakvova@gmail.com>
2024-03-24 17:06:59 +0200

    View image url

---
+++ Android/app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.myapp.application.HomeApplication;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        String url = "https://theadultman.com/wp-content/w3-webp/uploads/2021/06/High-value-woman_-Attractive-brunette-girl-in-yellow-top-smiling-.jpgw3.webp";
+
+        Glide.with(HomeApplication.getAppContext())
+                .load(url)
+                .apply(new RequestOptions().override(600))
+                .into(imageView);
+    }
+}
---
+++ Android/app/src/main/java/com/example/myapp/application/HomeApplication.java
@@ -0,0 +1,19 @@
+package com.example.myapp.application;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class HomeApplication extends Application {
+    private static Context appContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        appContext = getApplicationContext();
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
+
+    public static Context getAppContext() { return appContext; }
+}
